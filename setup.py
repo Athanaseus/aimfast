1506a437cef2efb07051146d6b95b943f4491e92
athanaseus <aramaila@ska.ac.za>
2018-01-10 14:15:55 +0200

    Package setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 from setuptools import setup, find_packages
+
+setup(name="image_fedility",
+      description="A Radio Astronomy Image Fidelity Examiner.",
+      author="Athanaseus Ramaila",
+      author_email="aramaila@ska.ac.za",
+      packages=find_packages(),
+      url='https://github.com/Athanaseus/examine-image-fidelity',
+      home_page='https://github.com/Athanaseus/examine-image-fidelity',
+      license="GNU GPL 3",
+      classifiers=["Intended Audience :: Developers",
+                   "Programming Language :: Python :: 2",
+                   "Topic :: Software Development :: Libraries :: Python Modules"],
+      platforms=["OS Independent"],
+      install_requires=["json",
+                        "numpy",
+                        "scipy",
+                        "astropy",
+                        "astLib"],
+      entry_points={'console_scripts': ['image-fidelity = image-fidelity.main']})
