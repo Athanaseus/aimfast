022966f1d71699c333f394a185613983d5801fe7
athanaseus <aramaila@ska.ac.za>
2018-01-24 19:21:10 +0200

    Mock modules fot doc purpose

--- doc/conf.py
+++ doc/conf.py
@@ -12,13 +12,23 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
+import sys
+import mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../'))
+# Modules to mock during auto doc to avoid installation
+# The ReadTheDocs FAQ says that you can setup a pip_requirements file to
+# install
+# any modules that are needed for your code, but this won’t work for any
+# modules
+# that include C code
+MOCK_MODULES = ['numpy', 'scipy', 'astropy', 'astLib']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
