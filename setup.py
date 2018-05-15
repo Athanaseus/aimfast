c00bd9628cd0faf6b496f469ceaa2d13b1b5a577
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2018-05-15 16:45:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="aimfast",
-      version="0.1.0",
+      version="0.1.1",
       description="An Astronomical Image Fidelity Assessment Tool.",
       author="Athanaseus Ramaila",
       author_email="aramaila@ska.ac.za",
