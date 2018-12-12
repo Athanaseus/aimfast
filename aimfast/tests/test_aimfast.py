b072ad6cc5736b5cc4bb11cc39991b9094cbc37b
athanaseus <aramaila@ska.ac.za>
2018-12-12 14:47:50 +0200

    Add unit tests

--- aimfast/tests/test_aimfast.py
+++ aimfast/tests/test_aimfast.py
@@ -1,2 +1,26 @@
-import time
-import unittest
+import numpy as np
+from aimfast import aimfast
+
+
+class TestClass(object):
+    "aimfast Test klass"
+    def test_deg2arcsec(self):
+        "Test deg2arcsec method"
+        input_value = 10.5
+        output_value = aimfast.deg2arcsec(input_value)
+        expected_value = input_value*3600.00
+        assert expected_value == output_value
+
+    def test_rad2deg(self):
+        "Test rad2deg method"
+        input_value = 1.5
+        output_value = aimfast.rad2deg(input_value)
+        expected_value = input_value*(180/np.pi)
+        assert expected_value == output_value
+
+    def test_rad2arcsec(self):
+        "Test rad2arcsec method"
+        input_value = 1.5
+        output_value = aimfast.rad2arcsec(input_value)
+        expected_value = input_value*(3600.0*180.0/np.pi)
+        assert expected_value == output_value
