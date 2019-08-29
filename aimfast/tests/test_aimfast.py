e9cd8fc50c10ceb20241f7b0eabb5241472b1d92
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-08-29 09:51:37 +0000

    Update tests and install no e

--- aimfast/tests/test_aimfast.py
+++ aimfast/tests/test_aimfast.py
@@ -68,6 +68,7 @@ class TestClass(object):
                           'SKEW': 0.186153,
                           'KURT': 2.870047,
                           'RMS': 3.1e-05,
+                          'MAD': 3.2e-05,
                           'STDDev': 3.1e-05,
                           'MEAN': 1.21497e-06}
         expected_normaltest_value = expected_value.pop('NORM')
