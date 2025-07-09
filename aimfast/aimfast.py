6df17046bc1298309dc80593defd4abe63f4d33b
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-07-09 13:19:05 +0200

    missing parentheses

--- aimfast/aimfast.py
+++ aimfast/aimfast.py
@@ -747,7 +747,7 @@ def image_dynamic_range(fitsname, residual, area_factor=6):
     global_std = residual_data[0, 0, ...].std()
     # Compute dynamic range
     DR = {
-        "deepest_negative"  : np.float64(peak_flux / abs(min_flux),
+        "deepest_negative"  : np.float64(peak_flux / abs(min_flux)),
         "local_rms"         : np.flaot64(peak_flux / local_std),
         "global_rms"        : np.float64(peak_flux / global_std),
     }
